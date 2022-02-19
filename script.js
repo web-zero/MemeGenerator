bd10dc73bd6c850882f28ddf1edb5791b2120503
Aditya <84168117+AdityaIyer2609@users.noreply.github.com>
2022-02-19 20:41:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var after='';
 
 function fetchmemes(){
-	color();
+	color()
 	
 	if(document.getElementById('memes')){
-		document.getElementById('memes').remove();
+		document.getElementById('memes').remove()
 	}
 	
-	let parentdiv=document.createELement('div');
+	let parentdiv=document.createELement('div')
 	parentdiv.id='memes'
 	fetch('https://www.reddit.com/r/memes.json?after=${after}')
 		.then(response=>response.json())
@@ -15,17 +15,17 @@ function fetchmemes(){
 		after=body.data.after
 		for(let index=0;index<body.data.children.length;index++){
 			if(body.data.children[index].data.post_hint=='image'){
-				let div=document.createELement('div');
-				let h4=document.createELement('h4');
-				let image=document.createELement('img');
-				image.src=body.data.children[index].data.url_overriden_by_dest;
-				h4.textContent=body.data.children[index].data.title;
-				div.appendChild(h4);
-				div.appendChild(image);
-				parentdiv.appendChild(div);
+				let div=document.createELement('div')
+				let h4=document.createELement('h4')
+				let image=document.createELement('img')
+				image.src=body.data.children[index].data.url_overriden_by_dest
+				h4.textContent=body.data.children[index].data.title
+				div.appendChild(h4)
+				div.appendChild(image)
+				parentdiv.appendChild(div)
 				
 			}
 		}
-		document.body.appendChild(parentdiv);
+		document.body.appendChild(parentdiv)
 	})
 }
