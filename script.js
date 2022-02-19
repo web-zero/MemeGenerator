da6993d3f6e6fc92e27816e2c39d02937d019b30
Aditya <84168117+AdityaIyer2609@users.noreply.github.com>
2022-02-19 20:24:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var after='';
+
+function fetchmemes(){
+	color();
+	
+	if(document.getElementById('memes')){
+		document.getElementById('memes').remove();
+	}
+	
+	let parentdiv=document.createELement('div');
+	parentdiv.id='memes'
+	fetch(' https://www.reddit.com/r/memes.json?after=${after}').then(response=>response.json()).then(body=>{
+		after=body.data.after
+		for(let index=0;index<body.data.children;index++){
+			if(body.data,children[index].data.post_hint=='iaage'){
+				let div=document.createELement('div');
+				let h4=document.createELement('h4');
+				let image=document.createELement('img');
+				image.src=body.data.children[index].data.url_overriden_by_dest;
+				h4.textContent=body.data.children[index].data.title;
+				div.appendChild(h4);
+				div.appendChild(image);
+				parentdiv.appendChild(div);
+				
+			}
+		}
+		document.body.appendChild(parentdiv);
+	})
+}
